88118f0b6d9b9445c58d72635514b15568feff8a
Turkushan <turkushan@gmail.com>
2026-06-01 21:45:36 +0200

    Rename project from ASA to GM Encoder
    Aligns naming with the GitHub repo (turkushan490/gm-encoder).
    
    File renames:
    - asa-gui.ps1   -> gm-encoder.ps1
    - asa-gui.cmd   -> gm-encoder.cmd
    
    Content updates across all source files:
    - 'ASA Video Encoder' / 'ASA Encoder' -> 'GM Encoder'
    - 'asa-gui'    -> 'gm-encoder'
    - 'AsaEncoder' -> 'GmEncoder' (settings.json path: %APPDATA%\GmEncoder\)
    - 'asa-install', 'asa-encoder', 'ASA-Encoder' all updated
    
    .gitignore updated for new artifact names. Title bar, build script
    output, README, LICENSE all reflect new project name.

--- asa-gui.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-REM ASA Encoder launcher - start GUI zonder console window
-REM Gebruik dit als asa-gui.exe nog niet gebouwd is.
-@powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0asa-gui.ps1"
